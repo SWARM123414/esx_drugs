7b336db906898a40216be9a43da7b112ed69c9a2
SWARM123414 <43252445+SWARM123414@users.noreply.github.com>
2019-03-26 00:50:41 +0000

    Add files via upload

---
+++ esx_drugs/config.lua
@@ -0,0 +1,39 @@
+Config              = {}
+Config.MarkerType   = 27
+Config.DrawDistance = 0
+Config.ZoneSize     = {x = 5.0, y = 5.0, z = 3.0}
+--Config.MarkerColor  = {r = 100, g = 204, b = 100}
+Config.RequiredCopsCoke = 1
+Config.RequiredCopsMeth = 3
+Config.RequiredCopsWeed = 0
+Config.RequiredCopsOpium = 4
+Config.Locale = 'en'
+
+Config.Zones = {
+	CokeFarm = 		 {x=-1565.77, y=4387.9, z=4.54},
+	CokeTreatment =  {x=1389.41, y=3604.69, z=38.94},
+	CokeResell = 	 {x=2432.22, y=4970.65, z=42.35},
+	
+--	MethFarm = 		 {x=2489.689, y=3443.108, z=49.06},
+--	MethTreatment =  {x=-1296.336, y=-1339.745, z=9.706},
+--	MethResell = 	 {x=-123.222, y=-1475.528, z=36.992},
+	
+	WeedFarm = 		 {x=2224.06, y=5577.03, z=53.85},
+	WeedTreatment =  {x=3561.54,  y=3673.14,  z=28.12},
+	WeedResell = 	 {x=2434.51, y=4968.45, z=42.35},
+	
+--	OpiumFarm = 	 {x=-276.8924,	 y=2210.816,  z=128.835},
+--	OpiumTreatment = {x=1593.74,y=3587.449, z=38.767},
+--	OpiumResell = 	 {x=1525.987,y=1710.205,z=110.004}
+}
+
+Config.Map = {
+ -- {name="Tratamento de coca",  color=1, scale=0.8, id=403, x=-1149.094, y=4939.743, z=222.269},
+ -- {name="Venda de coca",      color=1, scale=0.8, id=403, x=1506.506, y=-2136.203, z=76.477},
+ -- {name="Tratamento de Meta",  color=2, scale=0.8, id=403, x=-1296.336, y=-1339.745, z=9.706},
+ -- {name="Venda de Meta",      color=2, scale=0.8, id=403, x=-123.222, y=-1475.528, z=36.992},
+ -- {name="Tratamento de Opio", color=50, scale=0.8, id=403, x=1593.74,y=3587.449, z=38.767},
+ -- {name="Venda de Opio",     color=50, scale=0.8, id=403, x=1525.987,y=1710.205,z=110.004},
+ -- {name="Tratamento de Erva",  color=2, scale=0.8, id=140, x=2484.945,  y=3729.55,  z=42.895},
+ -- {name="Venda de Erva",      color=2, scale=0.8, id=140, x=-869.934, y=-1103.683, z=6.446},
+}
